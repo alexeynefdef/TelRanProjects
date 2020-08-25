4b8577e022d2faf8fdd83403eac9c106ae797448
alexeynefdef <flyrocker1@gmail.com>
2020-08-25 10:25:18 +0200

    last edit

--- Messenger/src/org/anefdef/StringSupplier.java
+++ Messenger/src/org/anefdef/StringSupplier.java
@@ -24,7 +24,6 @@ public class StringSupplier extends Thread {
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
-        this.interrupt();
     }
 }
 
