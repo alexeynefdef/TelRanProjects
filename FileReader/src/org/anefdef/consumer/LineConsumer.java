4b9dd9d78ec35a4ba14398621c86460e267d4831
alexeynefdef <flyrocker1@gmail.com>
2020-08-31 13:18:41 +0200

    edited class work

--- FileReader/src/org/anefdef/consumer/LineConsumer.java
+++ FileReader/src/org/anefdef/consumer/LineConsumer.java
@@ -9,6 +9,9 @@ import java.util.concurrent.BlockingQueue;
 public class LineConsumer extends Thread {
 
     public static final String STOP_COMMAND = "End of file";
+    static final String DELIMITER = "#";
+    static final String INVALID_LINE = "Invalid line format. No such delimiter: ";
+    static final String WRONG_OPERATION = "No such operation";
     BlockingQueue<String> queue;
     OperationStorage storage;
     BufferedWriter fileWriter;
@@ -28,24 +31,44 @@ public class LineConsumer extends Thread {
                 String line = queue.take();
                 if (line.equals(STOP_COMMAND))
                     return;
-                try {
-                    String[] splitLine = line.split("#");
-                    String text = splitLine[0];
-                    String operationName = splitLine[1];
-                    StringOperation operation = storage.getByName(operationName);
-                    String res = operation.operate(text);
-                    fileWriter.write(res);
-                    fileWriter.newLine();
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    fileWriter.write(line + ": Invalid line (No such delimiter #)");
-                    fileWriter.newLine();
-                } catch (NullPointerException e) {
-                    fileWriter.write(line + ": Invalid line (no such command)");
-                    fileWriter.newLine();
-                }
+                String result = processLine(line);
+
+                fileWriter.write(result);
+                fileWriter.newLine();
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Accepts line transforms it to another line.
+     * Line should contain two parts separated by delimiter{@code DELIMITER}.
+     * The first part is the text to operate on.
+     * The second part is the name of the operation to perform.
+     *
+     * @param line String in format <text>#<operation_name>
+     * @return transformed text according to the operation, if
+     * the line satisfies to the requirements.
+     * If the amount of parts in the line is not equal to two,
+     * then <line>" Invalid line" should return.
+     * If the operation not found , then
+     * <line>"#Operation No such operation" should return.
+     */
+    String processLine(String line) {
+        String[] splitLine = line.split(DELIMITER);
+        if (splitLine.length != 2) {
+            return line + " | " + INVALID_LINE + DELIMITER;
+        }
+
+        String text = splitLine[0];
+        String operationName = splitLine[1];
+
+        StringOperation operation = storage.getByName(operationName);
+
+        if (operation == null) {
+            return line + " | " + WRONG_OPERATION;
+        }
+        return operation.operate(text);
     }
 }
---
+++ FileReader/src/org/anefdef/consumer/ReverseOperationTest.java
@@ -0,0 +1,14 @@
+package org.anefdef.consumer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseOperationTest {
+
+    @Test
+    void testReverse_emptyString_emptyString() {
+
+    }
+
+}
