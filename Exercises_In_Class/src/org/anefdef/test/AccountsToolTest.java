5569d76ac4ba7c6d95fa9c026c62c9cfeba88284
alexeynefdef <flyrocker1@gmail.com>
2020-09-08 10:10:34 +0200

    hw

--- Exercises_In_Class/src/org/anefdef/test/AccountsToolTest.java
+++ Exercises_In_Class/src/org/anefdef/test/AccountsToolTest.java
@@ -32,6 +32,21 @@ class AccountsToolTest {
         }
     }
 
+
+    @Test
+    void testGetSumOfBlocked_3el_3blocked(){
+        Account a1 = new Account("1001",10_000);
+        Account a2 = new Account("1002",50_000);
+        Account a3 = new Account("1003",70_000);
+
+        List<Account> toTest = List.of(a1,a2,a3);
+
+        tool.blockTheOligarch(toTest,5000);
+        assertEquals(130000,tool.getSumOfBlocked(toTest));
+    }
+
+
+
     @Test
     void testBlockTheOligarch_3el_3unblocked(){
         Account a1 = new Account("1001",10_000);
