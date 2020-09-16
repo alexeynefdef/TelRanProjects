3f25b5c94e92121dd508e836347c57edad2e7f38
alexeynefdef <flyrocker1@gmail.com>
2020-09-16 16:15:14 +0200

    added homework

---
+++ Time_Homework/src/org/anefdef/Main.java
@@ -0,0 +1,11 @@
+package org.anefdef;
+
+import org.anefdef.task.NewYorker;
+
+public class Main {
+
+    public static void main(String[] args) {
+       NewYorker ny = new NewYorker();
+       System.out.println(ny.showTimeIntersectionsWithBerlin());
+    }
+}
