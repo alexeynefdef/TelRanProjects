8cb7a4829dcc347d100b484a7aa0e3ffd8003859
alexeynefdef <flyrocker1@gmail.com>
2020-09-16 20:50:37 +0200

    one more

--- Time_Homework/src/org/anefdef/Main.java
+++ Time_Homework/src/org/anefdef/Main.java
@@ -2,10 +2,13 @@ package org.anefdef;
 
 import org.anefdef.task.NewYorker;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 
     public static void main(String[] args) {
        NewYorker ny = new NewYorker();
-       System.out.println(ny.showTimeIntersectionsWithBerlin());
+       System.out.println(ny.showTimeIntersectionsWith("09:00:00","18:00:00","Europe/Berlin"));
     }
 }
--- Time_Homework/src/org/anefdef/task/NewYorker.java
+++ Time_Homework/src/org/anefdef/task/NewYorker.java
@@ -1,12 +1,29 @@
 package org.anefdef.task;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class NewYorker {
     public String showTimeIntersectionsWithBerlin() {
 
         ZonedDateTime givenBegin = ZonedDateTime.of(LocalDateTime.of(LocalDate.now(),LocalTime.of(9,0)),ZoneId.of("Europe/Berlin"));
         ZonedDateTime givenEnd = ZonedDateTime.of(LocalDateTime.of(LocalDate.now(),LocalTime.of(18,0)), ZoneId.of("Europe/Berlin"));
+        ZonedDateTime nyBegin = ZonedDateTime.of((LocalDateTime.of(LocalDate.now(),LocalTime.of(8,0))), ZoneId.of("US/Eastern"));
+        ZonedDateTime nyEnd = ZonedDateTime.of(LocalDateTime.of(LocalDate.now(),LocalTime.of(17,0)), ZoneId.of("US/Eastern"));
+        Duration begin = Duration.between(givenBegin,nyBegin);
+        Duration end = Duration.between(givenEnd,nyEnd);
+        LocalTime resultBegin = givenBegin.toLocalTime().plus(begin);
+        LocalTime resultEnd = givenEnd.toLocalTime().plus(end).minus(Duration.ofHours(5));
+        return "You can connect NY-Office during following hours: \n" +
+                resultBegin.toString() + " to " + resultEnd.toString();
+    }
+
+    public String showTimeIntersectionsWith(String workBegin, String workEnd, String zoneID) {
+
+        LocalTime beginTime = LocalTime.parse(workBegin, DateTimeFormatter.ISO_LOCAL_TIME);
+        LocalTime endTime = LocalTime.parse(workEnd,DateTimeFormatter.ISO_LOCAL_TIME);
+        ZonedDateTime givenBegin = ZonedDateTime.of(LocalDateTime.of(LocalDate.now(),beginTime),ZoneId.of(zoneID));
+        ZonedDateTime givenEnd = ZonedDateTime.of(LocalDateTime.of(LocalDate.now(),endTime), ZoneId.of("Europe/Berlin"));
         ZonedDateTime nyBegin = ZonedDateTime.of((LocalDateTime.of(LocalDate.now(),LocalTime.of(8,0))), ZoneId.of("US/Eastern"));
         ZonedDateTime nyEnd = ZonedDateTime.of(LocalDateTime.of(LocalDate.now(),LocalTime.of(17,0)), ZoneId.of("US/Eastern"));
         Duration begin = Duration.between(givenBegin,nyBegin);
