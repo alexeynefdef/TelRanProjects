42e800e56901618fa4d88892d7502e0af6da8d3a
alexeynefdef <flyrocker1@gmail.com>
2020-09-17 11:15:33 +0200

    edit2

--- Time_Homework/src/org/anefdef/Main.java
+++ Time_Homework/src/org/anefdef/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
        NewYorker ny = new NewYorker();
-       System.out.println(ny.showTimeIntersectionsWith("09:00:00","18:00:00","Europe/Berlin","08:00:00","17:00:00","America/Los_Angeles"));
+       System.out.println(ny.showTimeIntersectionsWith(
+                       "09:00:00", "18:00:00","Europe/Berlin",
+                       "08:00:00","17:00:00","America/Los_Angeles"));
     }
 }
