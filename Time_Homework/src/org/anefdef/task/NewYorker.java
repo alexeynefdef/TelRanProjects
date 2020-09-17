be5f5d965c864c11e8b307a32ca054ec01afd1f4
alexeynefdef <flyrocker1@gmail.com>
2020-09-17 11:17:49 +0200

    fin

--- Time_Homework/src/org/anefdef/task/NewYorker.java
+++ Time_Homework/src/org/anefdef/task/NewYorker.java
@@ -5,20 +5,6 @@ import java.time.format.DateTimeFormatter;
 
 public class NewYorker {
 
-    public String showTimeIntersectionsWithBerlin() {
-
-        ZonedDateTime givenBegin = ZonedDateTime.of(LocalDateTime.of(LocalDate.now(),LocalTime.of(9,0)),ZoneId.of("Europe/Berlin"));
-        ZonedDateTime givenEnd = ZonedDateTime.of(LocalDateTime.of(LocalDate.now(),LocalTime.of(18,0)), ZoneId.of("Europe/Berlin"));
-        ZonedDateTime nyBegin = ZonedDateTime.of((LocalDateTime.of(LocalDate.now(),LocalTime.of(8,0))), ZoneId.of("US/Eastern"));
-        ZonedDateTime nyEnd = ZonedDateTime.of(LocalDateTime.of(LocalDate.now(),LocalTime.of(17,0)), ZoneId.of("US/Eastern"));
-        Duration begin = Duration.between(givenBegin,nyBegin);
-        Duration end = Duration.between(givenEnd,nyEnd);
-        LocalTime resultBegin = givenBegin.toLocalTime().plus(begin);
-        LocalTime resultEnd = givenEnd.toLocalTime().plus(end).minus(Duration.ofHours(5));
-        return "You can connect NY-Office during following hours: \n" +
-                resultBegin.toString() + " to " + resultEnd.toString();
-    }
-
     public String showTimeIntersectionsWith(String workBegin1, String workEnd1, String zoneID1,String workBegin2, String workEnd2, String zoneID2) {
 
         LocalTime beginTime1 = LocalTime.parse(workBegin1, DateTimeFormatter.ISO_LOCAL_TIME);
