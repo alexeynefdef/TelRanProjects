d22a337b50f70b54c87eeee617de3536e9193292
alexeynefdef <flyrocker1@gmail.com>
2020-09-22 18:06:06 +0200

    edited load balancer

--- Load_Balancer/src/org/anefdef/LoadBalancer.java
+++ Load_Balancer/src/org/anefdef/LoadBalancer.java
@@ -8,23 +8,23 @@ import java.util.HashMap;
 
 public class LoadBalancer {
 
-    private static final int BACKEND_PORT = 4300;
-    private static final int GATEWAY_PORT = 3300;
+    private static final int BACKEND_PORT = 3300;
+    private static final int GATEWAY_PORT = 5001;
     private static final String GATEWAY_HOST = "localhost";
-    private static final HashMap<InetAddress, Integer> loadValuesByIP = new HashMap<>();
+    private static final HashMap<String, Integer> loadValuesByIP = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        receiveDataFromBackend();
-        sendCurrentAvailableAddressToGateway();
-    }
 
-    private static void receiveDataFromBackend() throws IOException {
+        // G E T T I N G   A D D R E S S   &    S E N D I N G   A D D R E S S
+
         //creating udp socket to get data from backend
         DatagramSocket backendReceivingSocket = new DatagramSocket(BACKEND_PORT);
         //container for incoming data from backend
-        byte[] backendData = new byte[8192];
+        byte[] backendData = new byte[1024];
         //creating datagram packet to receive from backend
         DatagramPacket backendIncomingPacket = new DatagramPacket(backendData, backendData.length);
+        //creating udp socket to send data to the gateway
+        DatagramSocket gatewaySendingSocket = new DatagramSocket(GATEWAY_PORT);
 
         while (true) {
             //receiving data from server
@@ -33,23 +33,12 @@ public class LoadBalancer {
             String rawData = new String(backendData, 0, backendIncomingPacket.getLength());
             //convert string to preferable format
             String[] addressPortLoadValue = rawData.split(":");
-            InetAddress address = InetAddress.getByName(addressPortLoadValue[0]);
+            String addressAndPort = addressPortLoadValue[0] + ":" + addressPortLoadValue[1];
             Integer loading = Integer.parseInt(addressPortLoadValue[2]);
             //save data to the storage
-            if (Integer.parseInt(addressPortLoadValue[1]) == GATEWAY_PORT) {
-                loadValuesByIP.put(address, loading);
-            }
-        }
-    }
-
-    private static void sendCurrentAvailableAddressToGateway() throws IOException {
-        //creating udp socket to send data to the gateway
-        DatagramSocket gatewaySendingSocket = new DatagramSocket(GATEWAY_PORT);
-        //finding best server
-        InetAddress bestServer = getBestServer();
-        //combining a string to send
-        String dataToSend = bestServer.toString();
-        while(true) {
+            loadValuesByIP.put(addressAndPort, loading);
+            //finding best server
+            String dataToSend = getBestServer();
             //creating a packet to send to the gateway
             DatagramPacket gatewaySendingPacket = new DatagramPacket(
                     dataToSend.getBytes(),
@@ -61,10 +50,13 @@ public class LoadBalancer {
         }
     }
 
-    private static InetAddress getBestServer() {
-
-        InetAddress best = null;
-        for (InetAddress address: loadValuesByIP.keySet()) {
+    /**
+     * Method, witch determines possible less loaded backend connection
+     * @return an address as a String <Address:PORT>
+     */
+    private static String getBestServer() {
+        String best = null;
+        for (String address: loadValuesByIP.keySet()) {
             if (loadValuesByIP.get(address) < loadValuesByIP.get(best)) {
                 best = address;
             }
