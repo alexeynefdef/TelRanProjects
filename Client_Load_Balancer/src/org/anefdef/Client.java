18ee5e28c00418c21060de4e69ffd04fb42eef04
alexeynefdef <flyrocker1@gmail.com>
2020-09-22 17:44:13 +0200

    small edit in client

--- Client_Load_Balancer/src/org/anefdef/Client.java
+++ Client_Load_Balancer/src/org/anefdef/Client.java
@@ -12,17 +12,20 @@ public class Client {
     static final int GATEWAY_PORT = 5000;
 
     public static void main(String[] args) throws IOException {
+
+        // getting line from keyboard
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String message = consoleReader.readLine();
+
         //creating connection with gateway
         Socket gatewaySocket = new Socket(GATEWAY_HOST,GATEWAY_PORT);
         //creating and sending messages to the gateway
         PrintStream socketOut = new PrintStream(gatewaySocket.getOutputStream());
-
+        //receiving data back from gateway
         BufferedReader socketIn = new BufferedReader(new InputStreamReader(gatewaySocket.getInputStream()));
 
-        String message= consoleReader.readLine();
         while (message != null && !message.equals("exit")) {
-            socketOut.println("Message from client");
+            socketOut.println(message);
             String response = socketIn.readLine();
             System.out.println(response);
         }
