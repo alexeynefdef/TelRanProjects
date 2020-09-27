e54f50622d97e96e46e9584015da96f6a25c5b97
alexeynefdef <flyrocker1@gmail.com>
2020-09-27 12:21:01 +0200

    added echo bot

---
+++ Tele_echo_bot/src/main/java/Telebot.java
@@ -0,0 +1,44 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class Telebot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init();
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try{
+            telegramBotsApi.registerBot(new Telebot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public String getBotUsername() {
+        return "anefdevweather_bot";
+    }
+
+    public String getBotToken() {
+        return "1251065115:AAFpo26yDK_HxnsUx6yeV1FEffoZxazApRE";
+    }
+}
